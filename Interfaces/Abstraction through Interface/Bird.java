0f17fa85c9c84f5b0d1b5b022a23882da3022386
OMkarTOdaramal <109520243+OMkarTOdaramal@users.noreply.github.com>
2025-12-25 19:32:54 +0530

    Abstraction through Interface

---
+++ Interfaces/Abstraction through Interface/Bird.java
@@ -0,0 +1,12 @@
+
+public interface  Bird {
+
+    public void fly();
+}
+
+class Eagle implements Bird{
+
+    public void fly(){
+        System.out.println("Eagle can fly");
+    }
+}
---
+++ Interfaces/Abstraction through Interface/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // We cannot create an object of interface but we can create its reference
+        Bird obj = new Eagle();
+        obj.fly();
+    }
+    
+}
